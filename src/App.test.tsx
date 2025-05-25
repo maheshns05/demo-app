794cfc5fe66fe548b49ae898e163f86dac969ea7
Mahesh <mahesh@Maheshs-MacBook-Air.local>
2025-05-24 23:24:43 -0400

    test case updates

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -5,6 +5,6 @@ import App from './App';
 describe('App', () => {
   it('Should render app component', () => {
     render(<App />);
-    expect(screen.getByText('Click on the Vite and React logos to learn more')).toBeInTheDocument();
+    expect(screen.getByText('Click on the Vite and React logos to learn more text')).toBeInTheDocument();
   });
 });
